8366a28845ea8b25760821cf6d670501f66e625f
ssunnykku <ssunnykku@gmail.com>
2026-04-07 16:55:23 +0900

    chore: CloudFront 도메인 주소 허용 코드 추가(cors)

--- src/main/java/com/sun/note/config/WebConfig.java
+++ src/main/java/com/sun/note/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://d31uruu1pa5ze8.cloudfront.net")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

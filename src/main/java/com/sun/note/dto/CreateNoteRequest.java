295bc140ce6225f18123f55e47edff1157f9d730
ssunnykku <ssunnykku@gmail.com>
2026-04-28 16:02:25 +0900

    fix: note 생성시 content @NotBlank -> @NotNull로 수정

--- src/main/java/com/sun/note/dto/CreateNoteRequest.java
+++ src/main/java/com/sun/note/dto/CreateNoteRequest.java
@@ -1,7 +1,5 @@
 package com.sun.note.dto;
 
-import java.util.UUID;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -9,7 +7,7 @@ import jakarta.validation.constraints.Size;
 public record CreateNoteRequest(
         Long categoryId,
         @NotBlank @Size(max = 255) String title,
-        @NotBlank @Size(max = 100000) String content) {
+        @NotNull @Size(max = 100000) String content) {
 
     public static CreateNoteRequest of(String title, String content) {
         return new CreateNoteRequest(null, title, content);
